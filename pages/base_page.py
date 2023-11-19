f2037fd83fe62440954351c3a3c32e133bd02cf5
Vlad Zharkov <zharkov.kill@yandex.by>
2023-11-19 17:45:31 +0300

    update ci/cd

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,13 +3,6 @@ import pytest
 import time
 import math
 import allure
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import NoSuchElementException
 from playwright.sync_api import Playwright, sync_playwright, expect
 from playwright.sync_api import Page
 
