da6e0da05b33af0654cfb0ed2ca0f3a558595010
Vlad Zharkov <zharkov.kill@yandex.by>
2023-11-19 17:58:26 +0300

    check allure report

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ from playwright.sync_api import Page,  Playwright, sync_playwright, expect
 @pytest.fixture(scope="function" , autouse=True)
 def page():
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(channel="chrome", headless=True , args=["--start-maximized"])
+        browser = playwright.chromium.launch(channel="chrome", headless=False , args=["--start-maximized"])
         context = browser.new_context(ignore_https_errors=True,no_viewport=True)
         page = context.new_page()
         yield page
