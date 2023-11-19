b89d763351484ad38bf678d4fc54dfafbb2325a3
Vlad Zharkov <zharkov.kill@yandex.by>
2023-11-19 17:47:42 +0300

    updare .gitignore

---
+++ pages/global_variables.py
@@ -0,0 +1,10 @@
+import pytest
+import allure
+import time
+from playwright.sync_api import Page, Playwright, sync_playwright, expect
+
+class GlobalVariables():
+    LINK_IC = "https://uat.israelcart.com/"
+
+
+
